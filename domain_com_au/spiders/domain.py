6cfeb1f64cc37819f7f912f9d6af9a095b3e749c
Pawel Zajzajer <czerwonykalafior@gmail.com>
2018-12-02 16:41:24 +0100

    Add bedroom filtering for lisings bigger than 400 items

--- domain_com_au/spiders/domain.py
+++ domain_com_au/spiders/domain.py
@@ -24,14 +24,24 @@ class DomainSpider(scrapy.Spider):
         @returns requests 8 16
         """
 
+        # E.g:. debug_region='All SA Suburbs'
+        debug_region = getattr(self, 'debug_region', None)
+
         soup = BeautifulSoup(response.body, 'html.parser')
-        region_links = soup.findAll('a', text=re.compile('All.*Suburbs'))
+
+        if debug_region is not None:
+            # TODO: TypeError no handled
+            pattern = debug_region
+        else:
+            pattern = 'All.*Suburbs'
+        region_links = soup.findAll('a', text=re.compile(pattern))
 
         for link in region_links:
             rel_url = link['href']
             # TODO: replace hardcoded '/buy/' in link, what if we want to scrape more in future?
             if '/buy/' not in rel_url:
                 continue
+            # TODO: response.follow can be used to skip urljoin with relative URLS
             yield scrapy.Request(url=urljoin(response.url, rel_url), callback=self.parse_regions)
 
     def parse_regions(self, response):
@@ -39,18 +49,30 @@ class DomainSpider(scrapy.Spider):
         2) Loop over links in 'ul class="suburbs-list"'
         and extract <suburb> from links  =  https://www.domain.com.au/sale/(.*?)/
 
-        @url https://www.domain.com.au/real-estate/rent/act/
-        @returns requests 130
+        @url https://www.domain.com.au/real-estate/buy/nsw/
+        @returns requests 4800
+
         """
+
+        # E.g:. debug_suburb_code='port-pirie-sa-5540'
+        debug_suburb_code = getattr(self, 'debug_suburb_code', None)
+
+        if debug_suburb_code is not None:
+            pattern = '(' + debug_suburb_code + ')'
+        else:
+            pattern = '/sale/(.*)/'
 
         soup = BeautifulSoup(response.body, 'html.parser')
         suburbs_links = soup.find("ul", class_="suburbs-list")
         suburbs_links = suburbs_links.find_all("a")
 
         for link in suburbs_links:
-            suburb = re.search('/sale/(.*)/', link['href'], re.IGNORECASE)
+            suburb = re.search(pattern, link['href'], re.IGNORECASE)
             # TODO: Lazy error handling
-            suburb = suburb.group(1) if suburb else ''
+            if suburb:
+                suburb = suburb.group(1)
+            else:
+                continue
 
             url = 'https://www.domain.com.au/sale/api/map-search/?suburb=' + suburb \
                   + '&sort=price-asc&mode=sale&digitaldata=1&displaymap=1'
@@ -59,10 +81,10 @@ class DomainSpider(scrapy.Spider):
 
     def parse_listing(self, response):
         """
-        Extract data from JSON response
+        Extract data from JSON response. Max no of location per 1 page returned = 200
 
-        @url https://www.domain.com.au/sale/api/map-search/?suburb=sydney-nsw-2000&sort=price-asc&mode=sale&digitaldata=1&displaymap=1
-        @returns items 200
+        @url https://www.domain.com.au/sale/api/map-search/?suburb=east-wardell-nsw-2477&sort=price-asc&mode=sale&digitaldata=1&displaymap=1
+        @returns items 4 8
         """
 
         data = json.loads(response.body)
@@ -71,18 +93,29 @@ class DomainSpider(scrapy.Spider):
         if search_type != 'Suburb':
             raise StopIteration
 
-        locs_found = data['digitalData']['searchResultCount']
+        no_of_locs_found = data['digitalData']['searchResultCount']
 
-        # TODO: FAILED. Filter by no. of bedrooms
-        if locs_found > 400:
+        if no_of_locs_found > 400 and '&bedrooms=' in response.url:
             raise CloseSpider(reason='Too much data in response')
 
+        # TODO: Below execution flow has a strong unpleasant smell
+
+        # When more than 400 we have to Filter by no. of bedrooms
+        if no_of_locs_found > 400 and '&bedrooms=' not in response.url:
+            bedrooms_no_params = ['0', '1', '2', '3', '4', '5-any']
+            for no_of_bedrooms in bedrooms_no_params:
+                url = response.url + '&bedrooms=' + no_of_bedrooms
+                yield scrapy.Request(url=url, callback=self.parse_listing)
+
         # Max loc on one page is 200. When there is more than 200 loc on listing changing sorting to DESC
-        # TODO: change to : 200 < locs_found < 400
-        elif locs_found > 200 and 'price-asc' in response.url:
+        elif no_of_locs_found > 200 and 'price-asc' in response.url:
+            for loc in data['results']:
+                yield loc
+
             url = response.url.replace('sort=price-asc', 'sort=price-desc')
             yield scrapy.Request(url=url, callback=self.parse_listing)
 
-        # TODO: Map Json to columns or store in MongoDB
-        for loc in data['results']:
-            yield loc
+        else:
+            # TODO: Map Json to columns or store in MongoDB
+            for loc in data['results']:
+                yield loc
